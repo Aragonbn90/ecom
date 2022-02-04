0cc93d7aab111bd5e78f0d43a81e2e0363e06ab3
Aragonbn90 <nghiabn1990@gmail.com>
2022-02-04 15:43:09 +0700

    Initial version of ecom generated by generator-jhipster@7.6.0

---
+++ src/main/java/com/ed/ecom/config/LoggingAspectConfiguration.java
@@ -0,0 +1,17 @@
+package com.ed.ecom.config;
+
+import com.ed.ecom.aop.logging.LoggingAspect;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import tech.jhipster.config.JHipsterConstants;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect(env);
+    }
+}
---
+++ src/main/java/com/ed/ecom/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.ed.ecom.repository;
+
+import com.ed.ecom.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
